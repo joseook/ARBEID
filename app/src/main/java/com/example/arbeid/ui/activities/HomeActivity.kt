37994e91d4aa7c7bc3eede3998c241e9ca0c6383
lucarl07 <junitinho2007@gmail.com>
2024-05-30 21:45:35 -0300

    :construction: feat: Add unsuccessful method to link each navbar item to a fragment

--- app/src/main/java/com/example/arbeid/ui/activities/HomeActivity.kt
+++ app/src/main/java/com/example/arbeid/ui/activities/HomeActivity.kt
@@ -1,12 +1,14 @@
 package com.example.arbeid.ui.activities
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.arbeid.R
 import com.example.arbeid.databinding.ActivityHomeBinding
+import com.example.arbeid.ui.fragments.TestFragment01
+import com.example.arbeid.ui.fragments.TestFragment02
 
 class HomeActivity : AppCompatActivity() {
 
@@ -22,5 +24,21 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.bottomNavigationBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(TestFragment01())
+                R.id.courses -> replaceFragment(TestFragment02())
+                else -> replaceFragment(TestFragment02())
+            }; true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val frManager = supportFragmentManager
+        val frTransaction = frManager.beginTransaction()
+
+        frTransaction.replace(R.id.fragment_container, fragment)
+        frTransaction.commit()
     }
 }
