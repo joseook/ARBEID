a1fa0a7c725406106ce1a8386895adce1e1ab0e0
lucarl07 <junitinho2007@gmail.com>
2024-05-31 20:43:50 -0300

    :bug: fix: Add intent to move from MainActivity to HomeActivity

--- app/src/main/java/com/example/arbeid/MainActivity.kt
+++ app/src/main/java/com/example/arbeid/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.example.arbeid
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.arbeid.ui.activities.HomeActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         enableEdgeToEdge()
 
         /* TODO: Create method for checking if the client user is currently logged */
         val isUserLogged = true
 
         if (isUserLogged) {
-            setContentView(R.layout.activity_home)
+            Intent(applicationContext, HomeActivity::class.java).also {
+                startActivity(it)
+            }
         } else {
-            /* TODO: Replace this with the login activity  */
-            setContentView(R.layout.activity_main)
+            /* TODO: Replace this with intent to access the login activity  */
         }
     }
 }
