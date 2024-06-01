744ec31602cf967a8b0d90f68aa0b6e2526d081a
lucarl07 <junitinho2007@gmail.com>
2024-05-31 21:07:26 -0300

    :lipstick: feat: Replace the default system bar padding method with enableEdgeToEdge()

--- app/src/main/java/com/example/arbeid/ui/activities/HomeActivity.kt
+++ app/src/main/java/com/example/arbeid/ui/activities/HomeActivity.kt
@@ -1,9 +1,11 @@
 package com.example.arbeid.ui.activities
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
 import com.example.arbeid.R
 import com.example.arbeid.databinding.ActivityHomeBinding
@@ -15,16 +17,11 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState); enableEdgeToEdge()
         binding = ActivityHomeBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
         replaceFragment(TestFragment01())
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         binding.bottomNavigationBar.setOnItemSelectedListener {
             when (it.itemId) {
